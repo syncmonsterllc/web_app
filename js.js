929e8dd8f5a4b4b34549b3fac6001ec17ab02514
syncmonster <papp.dany77@icloud.com>
2016-09-09 22:29:39 -0400

    initial testing

---
+++ js.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#close").click(function(){
+        $("#acc").hide();
+    });
+    $("#acc_open").click(function(){
+        $("#acc").show();
+    });
+    $("#acc_open").click(function(){
+        $("#close").hide();
+    });
+    $("#acc_open").click(function(){
+        $("#open").show();
+    });
+        
+});
